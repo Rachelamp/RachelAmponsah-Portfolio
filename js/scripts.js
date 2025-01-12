5d898faf751730840fc7a60119fed8985911a4f9
Rachelamp <157672647+Rachelamp@users.noreply.github.com>
2025-01-11 21:27:26 -0500

    Constructing flipbook

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#flipbook').turn({
+        width: 420,  // width of frames
+        height: 595, // height of frames
+        autoCenter: true,
+        elevation: 50,
+    });
+});
+
+console.log('jQuery version:', $.fn.jquery); // Check if jQuery is loaded
+console.log('Turn.js loaded:', typeof $.fn.turn); // Check if Turn.js is loaded
